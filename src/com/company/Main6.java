ed0a1348c164c126f6f4b3306fcd14bd0f067422
ArturPromon <artur.shovkunenko@gmail.com>
2021-09-07 18:58:36 +0300

    for + scanner

--- src/com/company/Main6.java
+++ src/com/company/Main6.java
@@ -1,7 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main6 {
     public static void main(String[] args) {
-        int granica = 65;
+        Scanner to_sto_vvodit_polzovatel = new Scanner(System.in);
+        int granica_pensii;
+        System.out.print("Vvedite granicu pensii: ");
+        granica_pensii = to_sto_vvodit_polzovatel.nextInt();
+        for(int i = 0;i<granica_pensii;i++){
+            System.out.println("Rabotaem poka nam " + i + " let");
+        }
+
     }
 }
