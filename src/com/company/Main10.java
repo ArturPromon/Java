8438be9268973758e1acef6993aaa111a97f951e
ArturPromon <artur.shovkunenko@gmail.com>
2021-09-13 17:03:42 +0300

    lesson 6

---
+++ src/com/company/Main10.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main10 {
+    public static void main(String[] args){
+        boolean isTrue = true;
+        int j = 0;
+        while(isTrue){
+            System.out.println("hello!");
+            j++;
+            if(j>10){
+                break;
+//                isTrue = false;
+
+            }
+        }
+    }
+}
---
+++ src/com/company/Main9.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main9 {
+    public static void main(String[] args){
+        int number;
+        number = 7;
+
+
+
+        switch(number){
+            case 5:
+                System.out.println("Number is 5");
+                break;
+            case 6:
+                System.out.println("number is 8");
+                break;
+            default:
+                System.out.print("number is not 5 or 6");
+        }
+    }
+}
